3a6efadaabf7743715a80dc84d8849e9ecce17df
bdream <bdletterbox@gmail.com>
2022-06-04 19:26:24 +0300

    Dummy generate random number logic was added.

--- src/App.svelte
+++ src/App.svelte
@@ -4,23 +4,67 @@
 	let min = 0;
 	let max = 100;
 
-	let randomValue = "Random";
+	let randomValue = "Number";
 
 	function getRandomInt(min, max) {
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
 	}
+
+	let newLine;
+	$: lines = [];
+
+	let randomLine = "Line";
+
+	function addNewLine(text) {
+		if (text) {
+			const newLine = {
+				text: text
+			};
+
+			lines = [...lines, newLine];
+		}
+	}
+
+	function getRandomLine(lines) {
+		let lineNumber = Math.floor(Math.random() * lines.length)
+		randomLine = (lineNumber + 1) + ". " + lines[lineNumber].text;
+	}
+
 </script>
 
 <main>
 	<h1>{name}</h1>
-	<input bind:value={min} placeholder="Enter min value">
-	<input bind:value={max} placeholder="Enter max value">
-	<button on:click={getRandomInt(min, max)}>
-		Push your luck
-	</button>
-	<h1>{randomValue}</h1>
+	<div>
+		<h2>Generate Random Number</h2>
+		<input bind:value={min} placeholder="Enter min value">
+		<input bind:value={max} placeholder="Enter max value">
+		<button on:click={getRandomInt(min, max)}>
+			Push your luck
+		</button>
+		<h2>{randomValue}</h2>
+	</div>
+	<div>
+		<h2>Get Random Line from the List</h2>
+		<input bind:value={newLine} placeholder="Enter your text">
+		<button on:click={addNewLine(newLine)}>
+			Add New Line
+		</button>
+		<div>
+			<ul>
+				{#each lines as { text }, i}
+					<li>
+						{i + 1}: {text}
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<button on:click={getRandomLine(lines)}>
+			Push your luck
+		</button>
+		<h2>{randomLine}</h2>
+	</div>
 </main>
 
 <style>
